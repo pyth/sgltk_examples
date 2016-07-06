31ed7021c6776f59452b1608956ab9a174bd1cd2
Fjodor Schelichow <fjodor.schelichow@hotmail.com>
2016-07-07 00:42:19 +0200

    system_info example

---
+++ system_info/main.cpp
@@ -0,0 +1,17 @@
+#include <sgltk/app.h>
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	sgltk::App::get_sys_info();
+	cout<<"System info"<<endl<<endl;
+	cout<<"OS: "<<sgltk::App::sys_info.platform_name<<endl;
+	cout<<"CPU Cores: "<<sgltk::App::sys_info.num_logical_cores<<endl;
+	cout<<"RAM: "<<sgltk::App::sys_info.system_ram<<" MB"<<endl;
+	cout<<"Highest supported OpenGL version: "<<
+		sgltk::App::sys_info.gl_version_major<<"."<<
+		sgltk::App::sys_info.gl_version_minor<<endl;
+	return 0;
+}
