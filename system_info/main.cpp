2061df07d9ffb3206bab14d04a79d7f095775413
Fjodor Schelichow <fjodor.schelichow@hotmail.com>
2016-07-08 00:40:04 +0200

    using new system information in the system_info example

--- system_info/main.cpp
+++ system_info/main.cpp
@@ -9,9 +9,28 @@ int main(int argc, char **argv) {
 	cout<<"System info"<<endl<<endl;
 	cout<<"OS: "<<sgltk::App::sys_info.platform_name<<endl;
 	cout<<"CPU Cores: "<<sgltk::App::sys_info.num_logical_cores<<endl;
-	cout<<"RAM: "<<sgltk::App::sys_info.system_ram<<" MB"<<endl;
-	cout<<"Highest supported OpenGL version: "<<
+	cout<<"RAM: "<<sgltk::App::sys_info.system_ram<<"MB"<<endl;
+	cout<<"Number of displays: "<<sgltk::App::sys_info.num_displays<<endl;
+
+	SDL_DisplayMode mode;
+	for(int i = 0; i < sgltk::App::sys_info.num_displays; i++) {
+		mode = sgltk::App::sys_info.desktop_display_modes[i];
+		cout<<"Display "<<i<<": "<<mode.w<<"x"<<mode.h<<" @"<<mode.refresh_rate<<endl;
+	}
+
+	cout<<"Supported OpenGL version: "<<
 		sgltk::App::sys_info.gl_version_major<<"."<<
-		sgltk::App::sys_info.gl_version_minor<<endl;
+		sgltk::App::sys_info.gl_version_minor<<endl<<endl;
+
+	int num_modes;
+	for(int i = 0; i < sgltk::App::sys_info.num_displays; i++) {
+		cout<<"Display "<<i<<" supports:"<<endl;
+		num_modes = sgltk::App::sys_info.supported_display_modes[i].size();
+		for(int j = 0; j < num_modes; j++){
+			mode = sgltk::App::sys_info.supported_display_modes[i][j];
+			cout<<mode.w<<"x"<<mode.h<<" @"<<mode.refresh_rate<<"Hz"<<endl;
+		}
+		cout<<endl;
+	}
 	return 0;
 }
