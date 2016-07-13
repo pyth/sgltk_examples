b245ed58f6daf94ee68a162f268071c6d60ae36e
Fjodor Schelichow <fjodor.schelichow@hotmail.com>
2016-07-13 15:35:34 +0200

    shader fix

--- loading_models/shaders/box_fs.glsl
+++ loading_models/shaders/box_fs.glsl
@@ -12,13 +12,13 @@ uniform sampler2D texture_diffuse;
 void main() {
 	vec4 tex = texture(texture_diffuse, tc);
 
-	vec3 eye = normalize(cam_vec);
+	vec3 cam = normalize(cam_vec);
 	vec3 norm = normalize(norm);
 	vec3 light = normalize(light_vec);
 	vec3 reflection = normalize(reflect(light, norm));
 
 	float LN = max(0.0, dot(norm, light));
-	float VR = max(0.0, dot(eye, reflection) * sign(LN));
+	float VR = max(0.0, dot(cam, reflection) * sign(LN));
 
 	float light_dist = length(light_vec);
 	float attenuation = 1.0 / (1.0 +
@@ -27,9 +27,9 @@ void main() {
 
 	vec4 amb = 0.2 * tex;
 
-	vec4 diff = vec4(LN * tex.xyz, 1);
+	vec4 diff = LN * tex;
 
-	vec4 spec = vec4(vec3(0.7 * pow(VR, 10)), 1);
+	vec4 spec = vec4(1,0,0,1) * pow(VR, 10);
 
-	color = amb + vec4(attenuation * (diff + spec).xyz, 1);
+	color = amb + vec4(attenuation * (diff.xyz + spec.xyz), 1);
 }
--- loading_models/shaders/box_vs.glsl
+++ loading_models/shaders/box_vs.glsl
@@ -13,15 +13,16 @@ uniform mat4 model_matrix;
 uniform mat4 view_matrix;
 uniform mat4 view_proj_matrix;
 uniform mat3 normal_matrix;
+
 uniform vec3 camera_pos;
 uniform vec3 light_pos;
 
 void main() {
 	vec4 pos = model_matrix * pos_in;
 
-	cam_vec = (view_matrix * vec4(camera_pos, 1) - pos).xyz;
-	light_vec = vec3(view_matrix * (vec4(light_pos, 1.0) - pos));
-	norm = vec3(view_matrix * vec4(normal_matrix * norm_in, 0));
+	cam_vec = -(view_matrix * pos).xyz;
+	light_vec = (view_matrix * (vec4(light_pos, 1) - pos)).xyz;
+	norm = (view_matrix * vec4(normal_matrix * norm_in, 0)).xyz;
 	tc = tex_coord_in0.xy;
 
 	gl_Position = view_proj_matrix * pos;
