04064ea8aae2138d549537d2e857141b48e66f4a
Fjodor Schelichow <fjodor.schelichow@hotmail.com>
2016-07-18 08:32:24 +0200

    adjusted the controller handling example to the library changes

--- controller_handling/main.cpp
+++ controller_handling/main.cpp
@@ -3,9 +3,6 @@
 
 using namespace std;
 
-//gamepad, player
-static map<int, int> player_map;
-
 int main(int argc, char **argv) {
 	sgltk::App::init();
 	SDL_Rect rect = sgltk::App::sys_info.display_bounds[0];
@@ -15,42 +12,19 @@ int main(int argc, char **argv) {
 }
 
 void sgltk::Window::handle_gamepad_added(unsigned int gamepad_id) {
-	bool found;
-	int player_max = 0;
-	int num_players = player_map.size();
-
-	//find highest player number
-	for(map<int, int>::iterator i = player_map.begin(); i != player_map.end(); i++) {
-		if(i->second > player_max)
-			player_max = i->second;
-	}
-	for(int i = 0; i < player_max; i++) {
-		found = false;
-		for(map<int, int>::iterator j = player_map.begin(); j != player_map.end(); j++) {
-			if(j->second == i) {
-				found = true;
-				break;
-			}
-		}
-		if(!found) {
-			player_map[gamepad_id] = i;
-			return;
-		}
-	}
-	player_map[gamepad_id] = num_players;
 }
 
 void sgltk::Window::handle_gamepad_removed(unsigned int gamepad_id) {
-	player_map.erase(gamepad_id);
 }
 
 void sgltk::Window::handle_gamepad_button(unsigned int gamepad_id, int button, bool pressed) {
-	std::cout<<"player "<<player_map[gamepad_id]<<": "<<button<<" "<<pressed<<std::endl;
-	play_rumble(gamepad_id, 0.5, 200);
+	std::cout<<"player "<<gamepad_id<<": "<<button<<" "<<pressed<<std::endl;
+	sgltk::Gamepad *gamepad = sgltk::Gamepad::id_map[gamepad_id];
+	gamepad->play_rumble(0.5, 200);
 }
 
 void sgltk::Window::handle_gamepad_axis(unsigned int gamepad_id, unsigned int axis, int value) {
-	std::cout<<"player "<<player_map[gamepad_id]<<": "<<axis<<" "<<value<<std::endl;
+	std::cout<<"player "<<gamepad_id<<": "<<axis<<" "<<value<<std::endl;
 }
 
 void sgltk::Window::handle_keyboard() {
