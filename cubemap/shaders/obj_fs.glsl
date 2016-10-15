ff8c35623d2efc0a61ca6af77c6d9f31e5a67971
Fjodor Schelichow <fjodor.schelichow@hotmail.com>
2016-10-15 23:16:30 +0200

    added refraction to the cubemap example

--- cubemap/shaders/obj_fs.glsl
+++ cubemap/shaders/obj_fs.glsl
@@ -1,15 +1,21 @@
 #version 130
 
-in vec3 pos;
+in vec3 cam_vec;
 in vec3 norm;
 
 out vec4 color;
 
-uniform vec3 cam_pos;
 uniform samplerCube texture_ambient;
 
 void main() {
-	vec3 cam_vec = -normalize(cam_pos - pos);
-	vec3 tc = normalize(reflect(cam_vec, normalize(norm)));
-	color = texture(texture_ambient, tc);
+	vec3 n = normalize(norm);
+	float CR = clamp(dot(cam_vec, n), 0.0, 1.0);
+
+	vec3 tc_refr = normalize(refract(cam_vec, n, 0.8));
+	vec3 tc_refl = -normalize(reflect(cam_vec, n));
+
+	vec4 refr = texture(texture_ambient, tc_refr);
+	vec4 refl = texture(texture_ambient, tc_refl);
+
+	color = mix(refl, refr, CR);
 }
--- cubemap/shaders/obj_vs.glsl
+++ cubemap/shaders/obj_vs.glsl
@@ -4,15 +4,17 @@ in vec4 pos_in;
 in vec3 tex_coord_in;
 in vec3 norm_in;
 
-out vec3 pos;
+out vec3 cam_vec;
 out vec3 norm;
 
+uniform vec3 cam_pos;
 uniform mat4 model_matrix;
 uniform mat4 view_proj_matrix;
 uniform mat3 normal_matrix;
 
 void main() {
 	norm = normal_matrix * norm_in;
-	pos = vec3(model_matrix * pos_in);
-	gl_Position = view_proj_matrix * vec4(pos, 1);
+	vec4 pos = model_matrix * pos_in;
+	cam_vec = normalize(cam_pos - pos.xyz);
+	gl_Position = view_proj_matrix * pos;
 }
