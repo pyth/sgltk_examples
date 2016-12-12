f86a33df5f209519512f38b62d0722107fb5eca4
Fjodor Schelichow <fjodor.schelichow@hotmail.com>
2016-12-12 04:02:46 +0100

    fixed the camera vector

--- cubemap/shaders/obj_fs.glsl
+++ cubemap/shaders/obj_fs.glsl
@@ -11,8 +11,8 @@ void main() {
 	vec3 n = normalize(norm);
 	float CR = clamp(dot(cam_vec, n), 0.0, 1.0);
 
-	vec3 tc_refr = normalize(refract(cam_vec, n, 0.8));
-	vec3 tc_refl = -normalize(reflect(cam_vec, n));
+	vec3 tc_refr = normalize(refract(-cam_vec, n, 0.5));
+	vec3 tc_refl = normalize(reflect(-cam_vec, n));
 
 	vec4 refr = texture(cubemap, tc_refr);
 	vec4 refl = texture(cubemap, tc_refl);
