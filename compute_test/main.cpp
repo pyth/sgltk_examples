ea2f8e38c4e23d7b3603b6ff222e5cf21b30f0b0
Fjodor Schelichow <fjodor.schelichow@hotmail.com>
2017-06-27 13:30:02 +0200

    the output buffer is now uninitialized prior to shader execution

--- compute_test/main.cpp
+++ compute_test/main.cpp
@@ -36,30 +36,32 @@ int main(int argc, char **argv) {
 
 	Win win("compute_test", 100, 100, 100, 100, SDL_WINDOW_HIDDEN);
 
-	std::vector<float> data = {100.f, 9.f, 16.f, 243.14f};
-	Buffer *data_buffer = new Buffer();
-	data_buffer->load(data, GL_STATIC_DRAW);
+	Buffer output;
+	Buffer data_buffer;
 
-	Buffer *output = new Buffer();
-	output->load(data, GL_STATIC_READ);
+	std::vector<float> data = {100.f, 9.f, 16.f, 243.14f};
+	data_buffer.load<float>(data, GL_STATIC_DRAW);
+	output.load<float>(data.size(), NULL, GL_STATIC_READ);
 
 	Shader comp_shader;
 	comp_shader.attach_file("shader_cs.glsl", GL_COMPUTE_SHADER);
 	comp_shader.link();
 
-	data_buffer->bind(GL_SHADER_STORAGE_BUFFER, 0);
-	output->bind(GL_SHADER_STORAGE_BUFFER, 1);
+	data_buffer.bind(GL_SHADER_STORAGE_BUFFER, 0);
+	output.bind(GL_SHADER_STORAGE_BUFFER, 1);
 	comp_shader.bind();
 	glDispatchCompute(data.size(), 1, 1);
 	glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
 
 	float out_data[4];
-	output->store(0, 4 * sizeof(float), &out_data);
+	output.store(0, 4 * sizeof(float), &out_data);
 	for(unsigned int i = 0; i < 4; i++)
 		std::cout << "square root of " << data[i] << " is " << out_data[i] << std::endl;
 
+#ifndef __linux__
 	int stop;
 	std::cin >> stop;
+#endif //__linux__
 
 	return 0;
 }
--- compute_test/shaders/shader_cs.glsl
+++ compute_test/shaders/shader_cs.glsl
@@ -2,7 +2,7 @@
 
 layout(std430, binding = 0) buffer data {float in_data[];};
 layout(std430, binding = 1) buffer outpt {float out_data[];};
-layout(local_size_x = 5, local_size_y = 1, local_size_z = 1) in;
+layout(local_size_x = 4, local_size_y = 1, local_size_z = 1) in;
 
 void main() {
 	out_data[gl_GlobalInvocationID.x] = sqrt(in_data[gl_GlobalInvocationID.x]);
